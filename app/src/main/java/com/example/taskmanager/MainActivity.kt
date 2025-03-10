a2071682ddc6ff2c889720e30389a748454b6750
96SLM <slmitchell2@pstcc.edu>
2025-03-10 14:35:27 -0400

    Reset functions to base function to be filled in Added an image to Resource Manager, added filler preview called "Seroronin" for motivation,

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -1,30 +1,17 @@
 package com.example.taskmanager
 
+//import androidx.compose.ui.Modifier
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.MaterialTheme.shapes
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
@@ -40,40 +27,27 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+//don't mind me
+@Composable
+fun Serotonin(modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.luffy)
+    Box (modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .size(100.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
 //Task Manager application that allows users to add tasks to a list and mark them as completed.
 
 //Composable for the TextField and "Add Task" button.
-//@Composable
-//fun TaskInputField(modifier: Modifier = Modifier) {
-//    val mediumPadding = dimensionResource(R.dimen.padding_medium)
-//    Card(
-//        modifier = modifier,
-//        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
-//    ) {
-//        Column(
-//            verticalArrangement = Arrangement.spacedBy(mediumPadding),
-//            horizontalAlignment = Alignment.CenterHorizontally,
-//            modifier = Modifier.padding(mediumPadding)
-//        ) {
-//            OutlinedTextField(
-//                value = "",
-//                singleLine = true,
-//                shape = shapes.large,
-//                modifier = Modifier.fillMaxWidth(),
-//                colors = TextFieldDefaults.textFieldColors(containerColor = colorScheme.surface),
-//                onValueChange = { },
-//                label = { Text(stringResource(R.string.enter_your_word)) },
-//                isError = false,
-//                keyboardOptions = KeyboardOptions.Default.copy(
-//                    imeAction = ImeAction.Done
-//                ),
-//                keyboardActions = KeyboardActions(
-//                    onDone = { }
-//                )
-//            )
-//        }
-//    }
-//}
+@Composable
+fun TaskInputField(/*modifier: Modifier = Modifier*/) {
+    
+}
 
 //Composable for displaying the list of tasks.
 //Store the list of tasks in a mutableStateListOf().
@@ -96,12 +70,13 @@ fun MainScreen(){
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TaskManagerPreview() {
     TaskManagerTheme {
-//        TaskInputField()
-//        TaskList()
-//        TaskItem()
-//        MainScreen()
+        Serotonin()
+        TaskInputField()
+        TaskList()
+        TaskItem()
+        MainScreen()
     }
 }
 /*ToBeDone
@@ -116,7 +91,7 @@ A Text element displaying the task description.
 A delete icon (IconButton) to remove the task.
 State Management
 
-Use remember,mutableStateOf, and mutableStateListOfto store:
+Use remember,mutableStateOf, and mutableStateListOf to store:
 The current text in the TextField.
 The list of tasks.
 When a task is added:
